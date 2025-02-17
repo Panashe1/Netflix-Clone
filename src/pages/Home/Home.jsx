8c270581207886d9d8cd6ffb8c86729bcd70aef1
Panashe1 <pmugayapi@hotmail.com>
2025-02-18 00:33:32 +1100

    Basic Hero Banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
+import hero_banner from "../../assets/hero_banner.jpg"
+import hero_title from "../../assets/hero_title.png"
+import play_icon from "../../assets/play_icon.png"
+import info_icon from "../../assets/info_icon.png"
 
-import Navbar from '../../components/Navbar/Navbar'
 const Home = () => {
   return (
-    <div className='home'>
-      <Navbar/>
+    <div className="home">
+      <Navbar />
+      <div className="hero">
+        <img className="banner-img" src={hero_banner} alt="" />
+        <div className="hero-caption">
+          <img className="hero-title" src={hero_title} alt="" />
+          <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+          <div className="hero-btns">
+          <button className="btn"><img src={play_icon} alt="" />Play</button>
+          <button className="btn dark-btn"><img src={info_icon} alt="" />More Info</button>
+        </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
