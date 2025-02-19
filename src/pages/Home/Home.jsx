a25c4eb666c199940bbae14e9774380cf34231ab
Panashe1 <pmugayapi@hotmail.com>
2025-02-19 20:43:18 +1100

    Added basic Tiles and movie section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
-import hero_banner from "../../assets/hero_banner.jpg"
-import hero_title from "../../assets/hero_title.png"
-import play_icon from "../../assets/play_icon.png"
-import info_icon from "../../assets/info_icon.png"
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
+import TitleCards from "../../components/TitleCards/TitleCards";
 
 const Home = () => {
   return (
@@ -14,12 +15,29 @@ const Home = () => {
         <img className="banner-img" src={hero_banner} alt="" />
         <div className="hero-caption">
           <img className="hero-title" src={hero_title} alt="" />
-          <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+          <p>
+            Discovering his ties to a secret ancient order, a young man living
+            in modern Istanbul embarks on a quest to save the city from an
+            immortal enemy.
+          </p>
           <div className="hero-btns">
-          <button className="btn"><img src={play_icon} alt="" />Play</button>
-          <button className="btn dark-btn"><img src={info_icon} alt="" />More Info</button>
+            <button className="btn">
+              <img src={play_icon} alt="" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="" />
+              More Info
+            </button>
+          </div>
+          <TitleCards/>
         </div>
-        </div>
+      </div>
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movies"}/>
+        <TitleCards title={"Action Movies"}/>
+        <TitleCards title={"Comedy Movies"}/>
+        <TitleCards title={"Rom Com Movies"}/>
       </div>
     </div>
   );
